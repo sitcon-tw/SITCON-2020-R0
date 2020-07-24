4e63b0b6b498ba257ca9bb7a23809c43cf0c4eb8
shibasalmon <shibasalmon@gmail.com>
2020-07-24 12:15:46 +0800

    [Debug] Fix time invarible.

--- src/components/NowAgendaBar/index.js
+++ src/components/NowAgendaBar/index.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react'
 
 export class NowAgendaBar extends Component {
 
+  constructor(props) {
+    super(props)
+    this.getTime = this.getTime.bind(this)
+    this.state = {
+      Time: '00:00'
+    }
+  }
+  componentDidMount() {
+    this.getTime()
+    setInterval(this.getTime, 5000)
+  }
   agendaLoader(e) {
     let agenda = e
     if (typeof e === 'object') {
@@ -20,7 +31,10 @@ export class NowAgendaBar extends Component {
     let now = new Date()
     let hours = now.getHours().toString().padStart(2, '0')
     let minutes = now.getMinutes().toString().padStart(2, '0')
-    return hours + ':' + minutes
+
+    let Time = hours + ':' + minutes
+
+    this.setState({ Time })
   }
 
   render() {
@@ -28,7 +42,7 @@ export class NowAgendaBar extends Component {
       return (
         <div className="NowAgendaBar">
           <div className="agenda">
-            <div className="agenda-time">{ this.getTime() }</div>
+            <div className="agenda-time">{ this.state.Time }</div>
             <div className="agenda-title">{ this.props.agenda.title }</div>
             <div className="agenda-name">{ this.props.agenda.name }</div>
           </div>

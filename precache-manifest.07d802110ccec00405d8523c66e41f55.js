f90dea1b3326a72cf12be7e3ce862841a3cccd5c
Denny Huang <denny0223@gmail.com>
2023-12-01 21:24:58 +0800

    3a31139 build

---
+++ precache-manifest.07d802110ccec00405d8523c66e41f55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d245ddd7205b9c00af83b26cb9bb60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46412fac214560fe3365",
+    "url": "./static/css/main.9c350461.chunk.css"
+  },
+  {
+    "revision": "ebc809b6d96fd3bab028",
+    "url": "./static/js/2.99d441d8.chunk.js"
+  },
+  {
+    "revision": "46412fac214560fe3365",
+    "url": "./static/js/main.39ef5a81.chunk.js"
+  },
+  {
+    "revision": "a75fb00c4e754f2a9372",
+    "url": "./static/js/runtime-main.29d30e65.js"
+  }
+]);
